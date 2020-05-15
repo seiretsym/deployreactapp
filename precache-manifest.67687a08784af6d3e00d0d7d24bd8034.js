9adf0c4f716420ae861716a8b8f087999585d41b
Kerwin Hy <kerwin.hy@gmail.com>
2020-05-15 02:20:46 -0700

    Updates

---
+++ precache-manifest.67687a08784af6d3e00d0d7d24bd8034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea2a340ac10e9ea1302c61fcd3d6c41",
+    "url": "/deployreactapp/index.html"
+  },
+  {
+    "revision": "c6118c5b67082d34d4c2",
+    "url": "/deployreactapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b7ce39b907a72b25a14",
+    "url": "/deployreactapp/static/js/2.d331aeca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deployreactapp/static/js/2.d331aeca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6118c5b67082d34d4c2",
+    "url": "/deployreactapp/static/js/main.1df12ff1.chunk.js"
+  },
+  {
+    "revision": "967b60611f83f8b30d10",
+    "url": "/deployreactapp/static/js/runtime-main.6f648e0c.js"
+  }
+]);
